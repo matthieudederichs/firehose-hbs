119c95aa3f61008c7c3b1e8b48d994dba089bcc6
Your Name <matthieu.dederichs@gmail.com>
2014-02-22 18:58:32 +0000

    my first commit for the hbs app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseHbs::Application.config.secret_key_base = '6e354eb62732ff64b9b7db46adf88f74830ee9deeb5097b31023a8a931d37ee5ec7e83a5b77f3b86879526b22eaa1e8b3fe792f23d0bb28df12a4763749fc5ee'
